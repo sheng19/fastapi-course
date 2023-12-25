036da4fc25480fe65762190cef1731a6a875194d
sheng <a282104@gmail.com>
2023-12-25 18:06:23 +0800

    Update 'routers/auth.py' to accept an instance of OAuth2PasswordRequestForm

--- oauth2.py
+++ oauth2.py
@@ -9,7 +9,7 @@ SECRET_KEY = "9a19eae78f5d4c774b8e03cd0168117f7a20f4ad3eae0d0d4fbfbc12500374f6"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from typing import Annotated
 
 import utils, schemas, oauth2
 from database import get_db
@@ -10,15 +12,15 @@ router = APIRouter(
     tags=['Auth']
 )
 
-@router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == user_credentials.email).first()
+@router.post("/login", response_model=schemas.Token)
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == form_data.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credential")
 
-    if not utils.verify(user_credentials.password, user.password):
+    if not utils.verify(form_data.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credential")
-
-    # create token
+    
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
