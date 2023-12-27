09e0479b6924bda2f665b2e044b3e2a5d28d866b
sheng <a282104@gmail.com>
2023-12-27 19:56:23 +0800

    Remove owner authorization on read_all_post endpoint

--- routers/post.py
+++ routers/post.py
@@ -11,8 +11,8 @@ router = APIRouter(
 
 
 @router.get('', status_code = status.HTTP_200_OK, response_model = list[schemas.Post])
-def read_all_post(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id)
+def read_all_post(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
     return posts
 
 @router.get('/{id}', status_code = status.HTTP_200_OK, response_model = schemas.Post)
